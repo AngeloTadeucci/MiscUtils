a93f49b6562274f6bf1f88b185415ef5ae6eac4e
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-11-26 22:01:46 +0100

    added an unbuffered streamreader and streamwriter

---
+++ MiscUtils/IO/UnbufferedStreamReader.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace MiscUtils.IO
+{
+    public class UnbufferedStreamReader : TextReader
+    {
+        private readonly bool LeaveOpen;
+
+        public Stream Stream { get; }
+
+        public UnbufferedStreamReader(Stream stream) : this(stream, false)
+        {
+
+        }
+
+        public UnbufferedStreamReader(Stream stream, bool leaveOpen)
+        {
+            this.Stream = stream;
+            this.LeaveOpen = leaveOpen;
+        }
+
+        public override int Peek()
+        {
+            int result = this.Stream.ReadByte();
+            this.Stream.Position--;
+
+            return result;
+        }
+
+        public override int Read()
+        {
+            return this.Stream.ReadByte();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (!this.LeaveOpen)
+                {
+                    this.Stream.Dispose();
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MiscUtils/IO/UnbufferedStreamWriter.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text;
+
+namespace MiscUtils.IO
+{
+    public class UnbufferedStreamWriter : TextWriter
+    {
+        private readonly bool LeaveOpen;
+
+        public Stream Stream { get; }
+
+        public override Encoding Encoding { get; }
+
+        public UnbufferedStreamWriter(Stream stream) : this(stream, Encoding.UTF8)
+        {
+
+        }
+
+        public UnbufferedStreamWriter(Stream stream, Encoding encoding) : this(stream, encoding, false)
+        {
+
+        }
+
+        public UnbufferedStreamWriter(Stream stream, Encoding encoding, bool leaveOpen)
+        {
+            this.Stream = stream;
+            this.Encoding = encoding;
+            this.LeaveOpen = leaveOpen;
+        }
+
+        public override void Write(char value)
+        {
+            byte[] bytes = this.Encoding.GetBytes(new char[] { value });
+            this.Stream.Write(bytes, 0, bytes.Length);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (!this.LeaveOpen)
+                {
+                    this.Stream.Dispose();
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
