ab498c987244ee20d3eee57e02be4e7ba13b2904
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-11-26 22:00:08 +0100

    added some byte array representation extensions

---
+++ MiscUtils/Extensions/Extensions.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Text;
+
+namespace MiscUtils
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Converts a byte array to its hex representation as text.
+        /// </summary>
+        /// <param name="byteArray">The byte array to be converted.</param>
+        /// <returns>The hex representation as text of the byte array.</returns>
+        public static string ToHexString(this byte[] byteArray)
+        {
+            return UnsafeByteLookup.ByteArrayToHexViaLookup32UnsafeDirect(byteArray);
+        }
+
+        /// <summary>
+        /// Converts a byte array to its hex representation as formatted text.
+        /// By default, 16 bytes per line are formatted, the line separator is "\n" and values separator is " ".
+        /// </summary>
+        /// <param name="byteArray">The byte array to be converted.</param>
+        /// <param name="bytesPerLine">Optional, the number of bytes formatted to one line of text.</param>
+        /// <param name="lineSeparator">Optional, the string that separates the lines.</param>
+        /// <param name="valueSeparator">Optional, the string that separates the values.</param>
+        /// <returns>The hex representation as formatted text of the byte array.</returns>
+        public static string ToFormattedHexString(this byte[] byteArray, byte bytesPerLine = 16, string lineSeparator = "\n", string valueSeparator = " ")
+        {
+            string hexData = ToHexString(byteArray);
+            int arrayLen = hexData.Length;
+            var sb = new StringBuilder();
+
+            int i = 0;
+            while (i < arrayLen)
+            {
+                sb.Append(hexData.Substring(i, 2));
+                sb.Append(valueSeparator);
+
+                i += 2;
+                if (i % bytesPerLine == 0 && i < arrayLen)
+                {
+                    if (i % (bytesPerLine * 2) == 0)
+                    {
+                        sb.Append(lineSeparator);
+                    }
+                    else
+                    {
+                        sb.Append(valueSeparator);
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Converts a byte to it's ASCII representation.
+        /// </summary>
+        /// <param name="b">The byte to be converted.</param>
+        /// <returns>An ASCII character.</returns>
+        public static char ToAsciiChar(this byte b)
+        {
+            if (b < 0x20 || b > 0x7E)
+            {
+                return (char)0x2E;
+            }
+            else
+            {
+                return (char)b;
+            }
+        }
+
+        /// <summary>
+        /// Converts a byte array to its hex representation as formatted text.
+        /// By default, 16 bytes per line are formatted, the line separator is "\n" and values separator is " ".
+        /// </summary>
+        /// <param name="byteArray">The byte array to be converted.</param>
+        /// <param name="bytesPerLine">Optional, the number of bytes formatted to one line of text.</param>
+        /// <param name="lineSeparator">Optional, the string that separates the lines.</param>
+        /// <param name="valueSeparator">Optional, the string that separates the values.</param>
+        /// <param name="asciiSeparator">Optional, the string that separates the ASCII characters.</param>
+        /// <param name="hexAndAsciiSeparator">Optional, the string that separates the hex and ASCII parts.</param>
+        /// <returns>The hex representation and ASCII representation as formatted text of the byte array.</returns>
+        public static string ToFormattedHexStringWithAscii(this byte[] byteArray, byte bytesPerLine = 16, string lineSeparator = "\n", string valueSeparator = " ", string asciiSeparator = "", string hexAndAsciiSeparator = "| ")
+        {
+            string formattedHexData = ToFormattedHexString(byteArray, bytesPerLine, lineSeparator, valueSeparator);
+            var sbWithAscii = new StringBuilder();
+
+            string[] lines = formattedHexData.Split(new[] { lineSeparator }, StringSplitOptions.None);
+            int firstLineLength = 0;
+
+            int i = 0;
+            while (i < lines.Length)
+            {
+                string line = lines[i];
+                if (firstLineLength == 0)
+                {
+                    firstLineLength = line.Length;
+                }
+
+                var sbAscii = new StringBuilder();
+                for (int j = i++ * bytesPerLine; j < i * bytesPerLine && j < byteArray.Length; j++)
+                {
+                    sbAscii.Append(byteArray[j].ToAsciiChar());
+                    sbAscii.Append(asciiSeparator);
+                }
+
+                sbWithAscii.Append(line.PadRight(firstLineLength));
+                sbWithAscii.Append(hexAndAsciiSeparator);
+                sbWithAscii.Append(sbAscii);
+                if (i < lines.Length)
+                {
+                    sbWithAscii.Append(lineSeparator);
+                }
+            }
+
+            return sbWithAscii.ToString();
+        }
+    }
+}
---
+++ MiscUtils/Unsafe/UnsafeByteLookup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MiscUtils
+{
+    /// <summary>
+    /// Lookup by byte unsafe (via CodesInChaos)
+    /// 
+    /// http://stackoverflow.com/questions/311165/how-do-you-convert-byte-array-to-hexadecimal-string-and-vice-versa/24343727#24343727
+    /// </summary>
+    unsafe class UnsafeByteLookup
+    {
+        private static readonly uint[] _lookup32Unsafe = CreateLookup32Unsafe();
+        private static readonly uint* _lookup32UnsafeP = (uint*)GCHandle.Alloc(_lookup32Unsafe, GCHandleType.Pinned).AddrOfPinnedObject();
+
+        private static uint[] CreateLookup32Unsafe()
+        {
+            uint[] result = new uint[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                string s = i.ToString("X2");
+
+                if (BitConverter.IsLittleEndian)
+                {
+                    result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+                }
+                else
+                {
+                    result[i] = ((uint)s[1]) + ((uint)s[0] << 16);
+                }
+            }
+
+            return result;
+        }
+
+        internal static string ByteArrayToHexViaLookup32UnsafeDirect(byte[] bytes)
+        {
+            uint* lookupP = _lookup32UnsafeP;
+            int arrayLen = bytes.Length;
+            string result = new string((char)0, arrayLen * 2);
+
+            fixed (byte* bytesP = bytes)
+            fixed (char* resultP = result)
+            {
+                var resultP2 = (uint*)resultP;
+
+                for (int i = 0; i < arrayLen; i++)
+                {
+                    resultP2[i] = lookupP[bytesP[i]];
+                }
+            }
+
+            return result;
+        }
+    }
+}
