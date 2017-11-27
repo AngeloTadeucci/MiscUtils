a721aff606c533a0cbaaf0289e23aa51ccd62aaf
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-11-28 00:42:23 +0100

    added a Path extended static helper class

---
+++ MiscUtils/IO/PathEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MiscUtils.IO
+{
+    public static class PathEx
+    {
+        public static string GetRootDirectory(string path)
+        {
+            path = path.Trim('/', '\\');
+
+            int index;
+            if ((index = path.IndexOfAny('/', '\\')) != -1)
+            {
+                return path.Substring(0, index);
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+    }
+}
