a0fe80e58d00756ba9d0d950b94fc437c3d2aebd
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-11-27 21:59:28 +0100

    added a string extension

---
+++ MiscUtils/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MiscUtils
+{
+    public static class StringExtensions
+    {
+        public static int IndexOfAny(this string s, params char[] anyOf)
+        {
+            return s.IndexOfAny(anyOf);
+        }
+    }
+}
